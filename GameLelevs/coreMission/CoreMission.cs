ba611d305276feba2b5c1bceb627e3012d8517b5
Dmitriy <savin_dmitriy.91@mail.ru>
2012-11-11 15:10:30 +0400

    Заглушка реализации движка миссий

--- GameLelevs/coreMission/CoreMission.cs
+++ GameLelevs/coreMission/CoreMission.cs
@@ -2,10 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
 
 namespace GameLevels.coreMission
 {
     class CoreMission : ICoreMission
     {
+        /// <summary>
+        /// Задание основной цели миссии
+        /// </summary>
+        public void SetMainMissionTarget() { 
+        }
+
+        /// <summary>
+        /// Задание второстепенных целей
+        /// </summary>
+        public void SetSecondaryMissionTarget() {
+        }
+
+        /// <summary>
+        /// Задание ключевой точки
+        /// </summary>
+        /// <param name="point">Точка</param>
+        /// <param name="effect">Эффект</param>
+        /// <returns>Удалось ли установить точку</returns>
+        public bool SetKeyPoint(Point point, Effect effect) {
+            return true;
+        }
     }
 }
